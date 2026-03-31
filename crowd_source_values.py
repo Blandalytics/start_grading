d0c756a3ef4dead9750dca6d336822a44f3514ec
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-03-31 11:25:16 -0700

    Update crowd_source_values.py

--- crowd_source_values.py
+++ crowd_source_values.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection 
+from pyfonts import set_default_font, load_google_font
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/14oyLIXsnDM4IZqGAca7mk5831_1WnxG2WEoSoILo5mg/edit?usp=sharing'
 
@@ -8,4 +9,4 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(spreadsheet=sheet_url)
 
-st.dataframe(df)
+st.dataframe(df,hide_index=True)
