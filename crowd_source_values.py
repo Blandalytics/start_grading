c3614c0f125c92ab3087e50f3ab7fc9dadf8be67
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-03-31 11:42:34 -0700

    Update crowd_source_values.py

--- crowd_source_values.py
+++ crowd_source_values.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection 
+import polars as pl
+import numpy as np
 from pyfonts import set_default_font, load_google_font
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/14oyLIXsnDM4IZqGAca7mk5831_1WnxG2WEoSoILo5mg/edit?usp=sharing'
@@ -7,6 +9,16 @@ sheet_url = 'https://docs.google.com/spreadsheets/d/14oyLIXsnDM4IZqGAca7mk5831_1
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(spreadsheet=sheet_url)
+game_line = pl.read_parquet('starter_games.parquet').sample()
 
-st.dataframe(df,hide_index=True)
+game_score = st.slider('Start Score',min_value=1, max_value=7,value=4)
+
+st.button("Submit", type="primary")
+if st.button("Submit Score"):
+    game_line = game_line.with_columns(pl.lit(game_score).alias("Score"))
+    game_line = conn.update(
+      worksheet="Responses",
+      data=game_line,
+      )
+    st.cache_data.clear()
+    st.rerun()
