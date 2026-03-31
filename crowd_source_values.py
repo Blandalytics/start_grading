e71a21f50888909ec4e4ec0f82323e2103bff129
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-03-31 11:49:44 -0700

    Update crowd_source_values.py

--- crowd_source_values.py
+++ crowd_source_values.py
@@ -10,7 +10,6 @@ sheet_url = 'https://docs.google.com/spreadsheets/d/14oyLIXsnDM4IZqGAca7mk5831_1
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 game_line = pl.read_parquet('starter_games.parquet').sample().with_columns(pl.all().round(2))
-game_line = game_line.with_columns(pl.lit(game_score).alias("Score"))
 st.dataframe(game_line)
 
 game_score = st.slider('Start Score',min_value=1, max_value=7,value=4)
