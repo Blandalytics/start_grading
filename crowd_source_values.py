5dcef0517ec6433cfd7023c5dfc336f1aff30d37
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2026-04-02 01:03:00 -0700

    Update crowd_source_values.py

--- crowd_source_values.py
+++ crowd_source_values.py
@@ -84,12 +84,22 @@ with col2:
             worksheet="Responses",
             ttl="5s"
         )
-        game_line = conn.update(
-          worksheet="Responses",
-          data=pd.concat([grade_df,
-                          game_line.reset_index(names='game_id')],
-                         ignore_index=True),
-          )
+        attempts = 0
+        while (grade_df.shape[0]==0) & (attempts <5):
+            st.write('Accessing database')
+            time.sleep(0.5)
+            grade_df = conn.read(
+                worksheet="Responses",
+                ttl="5s"
+            )
+            attempts += 1
+        if grade_df.shape[0]>0:
+            game_line = conn.update(
+                worksheet="Responses",
+                data=pd.concat([grade_df,
+                                game_line.reset_index(names='game_id')],
+                               ignore_index=True),
+              )
         st.cache_data.clear()
         del st.session_state['index']
         st.toast("Thanks for submitting a grade!", icon="✅")
